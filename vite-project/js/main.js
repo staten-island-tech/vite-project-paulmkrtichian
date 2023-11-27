42e555e49f51fc42db07afaf7d9471f1b8072d67
paulmkrtichian <paulmkr22@gmail.com>
2023-11-27 09:21:11 -0500

    11/27

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,91 @@
  import "../css/style.css";
  import toys from "./menu";
  const button = document.querySelector(".btn")
+ const toydata = document.querySelector("")
 
+ const toys = [
+  {
+    name: "LEGO Classic Creative Bricks Set",
+    type: "Building Blocks",
+    price: 25.99,
+    hasBatteries: "No",
+  },
+  {
+    name: "Barbie Dreamhouse",
+    type: "Dollhouse",
+    price: 99.99,
+    hasBatteries: "No",
+  },
+  {
+    name: "Hot Wheels 20 Car Gift Pack",
+    type: "Toy Cars",
+    price: 15.49,
+    hasBatteries: "No",
+  },
+  {
+    name: "Nerf N-Strike Elite Disruptor Blaster",
+    type: "Toy Gun",
+    price: 12.99,
+    hasBatteries: "No",
+  },
+  {
+    name: "Crayola Inspiration Art Case",
+    type: "Art Supplies",
+    price: 19.99,
+    hasBatteries: "No",
+  },
+  {
+    name: "Paw Patrol Ultimate Rescue Fire Truck",
+    type: "Action Figure Vehicle",
+    price: 49.99,
+    hasBatteries: "Yes",
+  },
+  {
+    name: "Sphero Mini App-Enabled Programmable Robot Ball",
+    type: "Robot Toy",
+    price: 44.99,
+    hasBatteries: "Yes",
+  },
+  {
+    name: "LeapFrog LeapPad Academy Kids' Learning Tablet",
+    type: "Educational Tablet",
+    price: 129.99,
+    hasBatteries: "Yes",
+  },
+  {
+    name: "Play-Doh Modeling Compound 10-Pack",
+    type: "Art and Craft",
+    price: 7.99,
+    hasBatteries: "No",
+  },
+  {
+    name: "VTech Kidizoom Smartwatch DX2",
+    type: "Electronic Toy",
+    price: 54.99,
+    hasBatteries: "Yes",
+  },
+  {
+    name: "LEGO Star Wars Millennium Falcon",
+    type: "Building Blocks",
+    price: 159.99,
+    hasBatteries: "No",
+  },
+  {
+    name: "American Girl Doll",
+    type: "Doll",
+    price: 129.95,
+    hasBatteries: "No",
+  },
+  {
+    name: "DJI RoboMaster S1 Educational Robot",
+    type: "Robot Kit",
+    price: 499.99,
+    hasBatteries: "Yes",
+  }
+]
+function createtoycard(toy){
+  const toydata = ``
+}
 
  const app = document.getElementById('app');
 
