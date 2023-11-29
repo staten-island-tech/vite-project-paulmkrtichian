04c0a101dc19b31a314a9a06e628a14949561c13
paulmkrtichian <paulmkr22@gmail.com>
2023-11-28 23:49:33 -0500

    11/28 2

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,135 +1,160 @@
  import "../css/style.css";
- const button = document.querySelector(".btn")
- 
+ const button = document.querySelector(".btn");
 
- const toys = [
-  {
-    name: "LEGO Classic Creative Bricks Set",
-    type: "Building Blocks",
-    price: 25.99,
-    hasBatteries: "No",
-   /* "img": "https://th.bing.com/th/id/OIP.LwsB9DSXijUSDoQkYiKqpgHaJQ?rs=1&pid=ImgDetMain"*/
-  },
-  {
-    name: "Barbie Dreamhouse",
-    type: "Dollhouse",
-    price: 99.99,
-    hasBatteries: "No",
-  },
-  {
-    name: "Hot Wheels 20 Car Gift Pack",
-    type: "Toy Cars",
-    price: 15.49,
-    hasBatteries: "No",
-  },
-  {
-    name: "Nerf N-Strike Elite Disruptor Blaster",
-    type: "Toy Gun",
-    price: 12.99,
-    hasBatteries: "No",
-  },
-  {
-    name: "Crayola Inspiration Art Case",
-    type: "Art Supplies",
-    price: 19.99,
-    hasBatteries: "No",
-  },
-  {
-    name: "Paw Patrol Ultimate Rescue Fire Truck",
-    type: "Action Figure Vehicle",
-    price: 49.99,
-    hasBatteries: "Yes",
-  },
-  {
-    name: "Sphero Mini App-Enabled Programmable Robot Ball",
-    type: "Robot Toy",
-    price: 44.99,
-    hasBatteries: "Yes",
-  },
-  {
-    name: "LeapFrog LeapPad Academy Kids' Learning Tablet",
-    type: "Educational Tablet",
-    price: 129.99,
-    hasBatteries: "Yes",
-  },
-  {
-    name: "Play-Doh Modeling Compound 10-Pack",
-    type: "Art and Craft",
-    price: 7.99,
-    hasBatteries: "No",
-  },
-  {
-    name: "VTech Kidizoom Smartwatch DX2",
-    type: "Electronic Toy",
-    price: 54.99,
-    hasBatteries: "Yes",
-  },
-  {
-    name: "LEGO Star Wars Millennium Falcon",
-    type: "Building Blocks",
-    price: 159.99,
-    hasBatteries: "No",
-  },
-  {
-    name: "American Girl Doll",
-    type: "Doll",
-    price: 129.95,
-    hasBatteries: "No",
-  },
-  {
-    name: "DJI RoboMaster S1 Educational Robot",
-    type: "Robot Kit",
-    price: 499.99,
-    hasBatteries: "Yes",
-  }
-]
-function createtoycard(toy){
-  const toydata = `
-<p>${toy.name}</p>
-<p>${toy.type}</p>
-<p>${toy.price}</p>
-<p>${toy.hasBatteries}</p>`
-  
-  productList.insertAjacentHTML('beforeend', toydata)
-}
+ document.addEventListener("DOMContentLoaded", function () {
+   const DOMSelectors = {
+     cardbox: document.getElementById("cardcontainer"),
+   };
+   const toys = [
+     {
+       name: "LEGO Classic Creative Bricks Set",
+       type: "Building Blocks",
+       price: 25.99,
+       hasBatteries: "No",
+       img: "https://m.media-amazon.com/images/I/91fLD7uWcaL.jpg",
+     },
+     {
+       name: "Barbie Dreamhouse",
+       type: "Dollhouse",
+       price: 99.99,
+       hasBatteries: "No",
+       img: "https://i5.walmartimages.com/asr/ad9c70bb-8be0-4910-842a-96b17775f10b.1203a4cea9dac438e5cc74491c41e1f4.jpeg",
+     },
+     {
+       name: "Hot Wheels 20 Car Gift Pack",
+       type: "Toy Cars",
+       price: 15.49,
+       hasBatteries: "No",
+       img: "https://m.media-amazon.com/images/I/71tF+RhyUXL.jpg",
+     },
+     {
+       name: "Nerf N-Strike Elite Disruptor Blaster",
+       type: "Toy Gun",
+       price: 12.99,
+       hasBatteries: "No",
+       img: "https://m.media-amazon.com/images/I/616YdwhmIbL._AC_UF894,1000_QL80_.jpg",
+     },
+     {
+       name: "Crayola Inspiration Art Case",
+       type: "Art Supplies",
+       price: 19.99,
+       hasBatteries: "No",
+       img: "https://m.media-amazon.com/images/I/91A9o5jh97L.jpg",
+     },
+     {
+       name: "Paw Patrol Ultimate Rescue Fire Truck",
+       type: "Action Figure Vehicle",
+       price: 50.0,
+       hasBatteries: "Yes",
+       img: "https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&itemId=1421241-847&recipeName=680",
+     },
+     {
+       name: "Sphero Mini App-Enabled Programmable Robot Ball",
+       type: "Robot Toy",
+       price: 44.99,
+       hasBatteries: "Yes",
+       img: "https://m.media-amazon.com/images/I/61Cc-wOpwiL._AC_UF894,1000_QL80_.jpg",
+     },
+     {
+       name: "LeapFrog LeapPad Academy Kids' Learning Tablet",
+       type: "Educational Tablet",
+       price: 129.99,
+       hasBatteries: "Yes",
+       img: "https://m.media-amazon.com/images/I/71yQZV2NEjL.jpg",
+     },
+     {
+       name: "Play-Doh Modeling Compound 10-Pack",
+       type: "Art and Craft",
+       price: 7.99,
+       hasBatteries: "No",
+       img: "https://images.freshop.com/00032245235008/b62b9cad49d9ce307124f77a6c46732d_large.png",
+     },
+     {
+       name: "VTech Kidizoom Smartwatch DX2",
+       type: "Electronic Toy",
+       price: 54.99,
+       hasBatteries: "Yes",
+       img: "https://m.media-amazon.com/images/I/81FbghVRGbL._AC_UF894,1000_QL80_.jpg",
+     },
+     {
+       name: "LEGO Star Wars Millennium Falcon",
+       type: "Building Blocks",
+       price: 159.99,
+       hasBatteries: "No",
+       img: "https://m.media-amazon.com/images/I/81kmuPJ9IOL.jpg",
+     },
+     {
+       name: "American Girl Doll",
+       type: "Doll",
+       price: 129.95,
+       hasBatteries: "No",
+       img: "https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&itemId=1662177-847&recipeName=680",
+     },
+     {
+       name: "DJI RoboMaster S1 Educational Robot",
+       type: "Robot Kit",
+       price: 499.99,
+       hasBatteries: "Yes",
+       img: "https://m.media-amazon.com/images/I/511fHhHG13L._AC_UF894,1000_QL80_.jpg",
+     },
+   ];
+   function renderProducts(productList) {
+     DOMSelectors.cardbox.innerHTML = "";
+     productList.forEach((toy) => {
+       const stuff = `
+      <div class="cardcontainer">
+        <h2>${toy.name}</h2>
+        <h3>$${toy.price}</p>
+        <img class="collection" src="${toy.img}" alt="">
+        <p>BATTERIES?: ${toy.hasBatteries}</p>
+        <p>TYPE: ${toy.type}</p>
+        <p> ---------------------------------------------------------------------------------------------- </p>
+      </div>`;
+       DOMSelectors.cardbox.insertAdjacentHTML("beforeend", stuff);
+     });
+   }
 
- const app = document.getElementById('app');
+   toys.forEach((toy) => {
+     const stuff = `
+    <div class="cardcontainer">
+      <h2>${toy.name}</h2>
+      <h3>$${toy.price}</p>
+      <img class="collection" src="${toy.img}" alt="">
+      <p>BATTERIES?: ${toy.hasBatteries}</p>
+      <p>TYPE: ${toy.type}</p>
+      <p> ----------------------------------------------------------------------------------------------</p>
+    </div>`;
+     DOMSelectors.cardbox.insertAdjacentHTML("beforeend", stuff);
+   });
 
-function renderProducts(productList) {
-  app.innerHTML = '';
-  productList.forEach((toy) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `${toy.name}, ${toy.price} ${toy.img}`;
-    app.appendChild(card);
-  });
-}
-renderProducts(toys);
+   document.querySelector(".btn2").addEventListener("click", function () {
+     const cheapToys = toys.filter((toy) => toy.price < 50);
+     renderProducts(cheapToys);
+   });
 
-document.querySelector(".btn").addEventListener("click", function () {
-if (document.body.classList.contains("cold")){
-    document.body.classList.add("warm");
-    document.body.classList.remove("cold");
-    button.textContent = "Change to Cold"
-} else {
-    document.body.classList.add("cold");
-    document.body.classList.remove("warm")
-    button.textContent = "Change to Warm"
-}
+   document.querySelector(".btn3").addEventListener("click", function () {
+     const cheapToys = toys.filter(
+       (toy) => toy.price >= 50 && toy.price <= 100
+     );
+     renderProducts(cheapToys);
+   });
 
-})
+   document.querySelector(".btn4").addEventListener("click", function () {
+     const cheapToys = toys.filter((toy) => toy.price > 100);
+     renderProducts(cheapToys);
+   });
 
-document.querySelector(".btn2").addEventListener("click", function ()  {
-  const filteredProducts = toys.filter(toy => toy.price < 50);
-  renderProducts(filteredProducts);
-});
+   const app = document.getElementById("app");
 
-document.querySelector(".btn3").addEventListener("click", function ()  {
-  const filteredProducts = toys.filter(toy => toy.price >= 50 && toy.price <= 100);
-  renderProducts(filteredProducts);
-});
-
-document.querySelector(".btn4").addEventListener("click", function ()  {
-  const filteredProducts = toys.filter(toy => toy.price > 100);
-  renderProducts(filteredProducts);
-});
+   document.querySelector(".btn").addEventListener("click", function () {
+     if (document.body.classList.contains("cold")) {
+       document.body.classList.add("warm");
+       document.body.classList.remove("cold");
+       button.textContent = "Change to Cold";
+     } else {
+       document.body.classList.add("cold");
+       document.body.classList.remove("warm");
+       button.textContent = "Change to Warm";
+     }
+   });
+ });
